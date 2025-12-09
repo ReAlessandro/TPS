f439074e4bae7d18f177cdadc73c6f1041269202
ReAlessandro <re.alessandro@itiscannizzaro.edu.it>
2025-12-09 18:53:53 +0100

    forse report finale

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -1,222 +1,96 @@
-function LetturaFileXml() {
+function pulisciTabella() {
+    document.getElementById("tabella").innerHTML =
+        "<tr><th>Nome</th><th>Prezzo</th><th>Descrizione</th><th></th></tr>";
+}
+
+function LetturaFileXML() {
+    pulisciTabella();
+
     let leggi = new XMLHttpRequest();
     leggi.open('GET', 'https://realessandro.github.io/TPS/e-commerce/Lavoro.xml', true);
     leggi.send();
 
     leggi.onload = function () {
       const xml = leggi.responseXML;
-      if (!xml) {
-        document.getElementById("t1").innerHTML = "Errore nel caricamento del file XML.";
-        return;
-      }
+      
 
-      const prodotti = xml.getElementsByTagName("Prodotti");
-      let str = "";
+      const prodotto = xml.getElementsByTagName("prodotto");
 
       for (let i = 0; i < prodotti.length; i++) {
-        let nome = prodotti[i].getElementsByTagName("nome")[0].textContent;
-        let prezzo = parseInt(prodotti[i].getElementsByTagName("prezzo")[0].textContent);
-        let descrizione = prodotti[i].getElementsByTagName("descrizione")[0].textContent;
+        let nome = prodotto[i].getElementsByTagName("nome")[0].textContent;
+        let prezzo = prodotto[i].getElementsByTagName("prezzo")[0].textContent;
+        let descrizione = prodotto[i].getElementsByTagName("descrizione")[0].textContent;
 
 
-       str = str + "<tr><td>" + nome + "</td><td>" + prezzo + "</td><td>" + descrizione + "</td><td></tr>";
+        aggiungiriga(nome, prezzo, descrizione);
       }
 
-      str += "</table>";
-      document.getElementById("catalogo").innerHTML = str;
-    };
+    }
 }
 
-//Lettura JSON 
-function LetturaFileJson() {
-      let leggi = new XMLHttpRequest();
 
-      leggi.open('GET', 'https://realessandro.github.io/TPS/e-commerce/elettrodomestici.json', true);
-      leggi.send();
-      leggi.onload = function () {
-        var str = "";
-        const dati = JSON.parse(leggi.responseText);
-        for (i = 0; i < dati.prodotti.length; i++) {
-            let nome = dati.prodotti[i].nome;
-            let prezzo = dati.prodotti[i].prezzo;
-            let descrizione = dati.prodotti[i].descrizione;
+function LetturaFileJSON() {
+    pulisciTabella();
 
-         
+    let leggi = new XMLHttpRequest();
 
-          str = str + "<tr><td>" + dati.prodotti[i].nome + "</td><td>" + dati.prodotti[i].prezzo + "</td><td>" + dati.prodotti[i].descrizione + "</td><td></tr>";
-        }
-
-        str = str + "</table>";
-
-
-
-        document.getElementById("catalogo").innerHTML = str;
+    leggi.open('GET', 'https://realessandro.github.io/TPS/e-commerce/elettrodomestici.json', true);
+    leggi.send();
+    leggi.onload = function () {
         
-      }
-}
+    const dati = JSON.parse(leggi.responseText);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var catalogo = {};
-var carrello = [];
-
-// ======= XML =======
-fetch("lavoro.xml").then(r => r.text()).then(dati => {
-    var xml = new DOMParser().parseFromString(dati, "text/xml");
-    var lista = xml.getElementsByTagName("prodotto");
-    catalogo["Lavoro"] = [];
-
-    for (var i = 0; i < lista.length; i++) {
-        var nome = lista[i].getElementsByTagName("nome")[0].textContent;
-        var prezzo = lista[i].getElementsByTagName("prezzo")[0].textContent;
-        var descrizione = lista[i].getElementsByTagName("descrizione")[0].textContent;
-        catalogo["lavoro"].push({ nome: nome, prezzo: prezzo, descrizione: descrizione});
-    }
-});
-
-// ======= JSON =======
-fetch("elettrodomestici.json").then(r => r.json()).then(dati => {
-    catalogo["elettrodomestici"] = dati;
-});
-
-// ======= CSV =======
-fetch("sport.csv").then(r => r.text()).then(dati => {
-    var righe = dati.split("\n");
-    catalogo["sport"] = [];
-
-    for (var i = 1; i < righe.length; i++) {
-        var campi = righe[i].split(",");
-        if (campi.length > 1) {
-            catalogo["sport"].push({
-                nome_prodotto: campi[1],
-                prezzo: campi[2],
-                descrizione: campi[3]
-            });
+        for (i = 0; i < dati.prodotti.length; i++) {
+            let x = dati.prodotti[i];
+            aggiungiriga(x.nome, x.prezzo, x.descrizione);
         }
     }
-});
+}
 
-// ======= TXT =======
-fetch("aerei.txt").then(r => r.text()).then(dati => {
-    var righe = dati.split("\n");
-    catalogo["Aerei"] = [];
+function LetturaFileCSV() {
 
-    for (var i = 0; i < righe.length; i++) {
-        if (righe[i].includes("|")) {
-            var pezzi = righe[i].split("|");
-            catalogo["Aerei"].push({
-                nome: pezzi[0].trim(),
-                prezzo: pezzi[1],
-                descrizione: pezzi[2]
-            });
+    pulisciTabella();
+
+    let leggi = new XMLHttpRequest();
+    leggi.open('GET', 'https://realessandro.github.io/TPS/e-commerce/sport.csv', true);
+    leggi.send();
+    leggi.onload = function () {
+        var str = "";
+        const righe = leggi.responseText.split("\n");
+        for (let i = 1; i < righe.length; i++) {
+            const colonne = righe[i].split(",");
+            aggiungiriga(colonne[0], colonne[1], colonne[2]);
         }
     }
-});
+}
 
-// ======= MOSTRA CATALOGO =======
-setTimeout(function () {
-    var div = document.getElementById("catalogo");
+function LetturaFileTXT(){
+    pulisciTabella();
 
-    for (var categoria in catalogo) {
-        var btn = document.createElement("button");
-        btn.innerHTML = categoria;
+    let leggi = new XMLHttpRequest();
+    leggi.open('GET', 'https://realessandro.github.io/TPS/e-commerce/aerei.txt', true);
+    leggi.send();
 
-        btn.onclick = function () {
-            mostraProdotti(this.innerHTML);
-        };
+    leggi.onload = function () {
+        const righe = leggi.responseText.split("\n");
 
-        div.appendChild(btn);
-    }
-}, 1000);
-
-// ======= MOSTRA PRODOTTI =======
-function mostraProdotti(cat) {
-    document.getElementById("titolo").innerHTML = cat;
-    var div = document.getElementById("prodotti");
-    div.innerHTML = "";
-
-    for (var i = 0; i < catalogo[cat].length; i++) {
-        var p = catalogo[cat][i];
-
-        div.innerHTML += 
-            p.nome + " - €" + p.prezzo +
-            " <button onclick=\"aggiungi('" + p.nome + "', " + p.prezzo + ")\">Acquista</button><br>";
+        for (let i = 0; i < righe.length; i++) {
+            const colonne = righe[i].split(";");
+            aggiungiriga(colonne[0], colonne[1], colonne[2]);
+        }
     }
 }
 
-// ======= CARRELLO =======
-function aggiungi(nome, prezzo) {
-    carrello.push({ nome: nome, prezzo: prezzo });
-    mostraCarrello();
+function acquista(nome, prezzo) {
+    alert("Hai acquistato: " + nome + " (€" + prezzo + ")");
 }
 
-function mostraCarrello() {
-    var div = document.getElementById("carrello");
-    div.innerHTML = "";
-    var totale = 0;
+function aggiungiriga(nome, prezzo, descrizione) {
+    const tabella = document.getElementById("tabella");
 
-    for (var i = 0; i < carrello.length; i++) {
-        div.innerHTML += carrello[i].nome + " - €" + carrello[i].prezzo + "<br>";
-        totale += parseFloat(carrello[i].prezzo);
-    }
-
-    div.innerHTML += "<b>TOTALE: €" + totale + "</b><br>";
-    div.innerHTML += "<button onclick='generaPDF()'>Conferma</button>";
+    tab.innerHTML +=
+        "<tr><td>" + nome + "</td><td>" + prezzo + "</td><td>" + descrizione +
+        "</td><td><button onclick='acquista(\"" + nome + "\", \"" + prezzo + "\")'>Acquista</button></td></tr>";
 }
 
-// ======= PDF =======
-function generaPDF() {
-    var doc = new window.jspdf.jsPDF();
-    var y = 20;
 
-    doc.text("SCONTRINO", 20, y);
-    y += 10;
-
-    var totale = 0;
-
-    for (var i = 0; i < carrello.length; i++) {
-        doc.text(carrello[i].nome + " €" + carrello[i].prezzo, 20, y);
-        totale += parseFloat(carrello[i].prezzo);
-        y += 10;
-    }
-
-    y += 10;
-    doc.text("TOTALE: €" + totale, 20, y);
-
-    doc.save("scontrino.pdf");
-    carrello = [];
-    mostraCarrello();
-}
