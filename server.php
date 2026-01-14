ca1fdaa851d06b007aded4cf025d015e997abf03
ReAlessandro <re.alessandro@itiscannizzaro.edu.it>
2026-01-14 23:27:38 +0100

    final RSA

--- server.php
+++ server.php
@@ -1,3 +1,17 @@
 <?php
- 
+$C = $_GET["cifrato"];
+
+echo "Messaggio cifrato: $C";
+$file = fopen("priv.json", "r");
+$nuovo = fread($file, filesize("priv.json"));
+echo $nuovo;
+$contenuto = json_decode($nuovo);
+
+$K = $contenuto->K;
+$N = $contenuto->N;
+function decifrare(int $C, int $K, int $N){
+    return pow($C, $K) % $N;
+}
+
+echo "<br>Messaggio decifrato: " . decifrare($C, $K, $N);
 ?>
